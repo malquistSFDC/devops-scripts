1a07911ac21900d2cfca81565e09faf1d1f5f981
malquistSFDC <malquist@salesforce.com>
2025-11-05 09:56:48 -0500

    Additional project configs

---
+++ .sfdx/typings/lwc/customlabels.d.ts
@@ -0,0 +1,8 @@
+declare module "@salesforce/label/c.quoteManual" {
+    var quoteManual: string;
+    export default quoteManual;
+}
+declare module "@salesforce/label/c.quoteAuto" {
+    var quoteAuto: string;
+    export default quoteAuto;
+}
